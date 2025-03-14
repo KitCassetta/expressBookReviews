a8b57e9468b8557b40fc2ad0f0bf0445d480eaf1
kit cassetta <kitcassetta@gmail.com>
2025-03-14 14:21:11 -0400

    task 2-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,27 +12,45 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const bookKeys = Object.keys(books);
+  
+    for (let key of bookKeys) {
+      if (books[key].author === author) {
+        return res.send(books[key]);
+      }
+    }
+  
+    // If no match is found
+    return res.status(404).send({ message: "Author not found" });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    const bookKeys = Object.keys(books);
+  
+    for (let key of bookKeys) {
+      if (books[key].title === title) {
+        return res.send(books[key]);
+      }
+    }
+  
+    // If no match is found
+    return res.status(404).send({ message: "Book not found" });
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
